871d56e098d493d9a512acd93727830c9e64a60f
Gohar Gevorgyan <gohargevorgyan95@gmail.com>
2017-09-28 00:37:25 +0400

    Initial

---
+++ Convert/Convert/Methods.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert
+{
+    class Methods
+    {
+       
+
+       public static int ConvertKilometrToMetr(int arg)
+        {
+            
+            return arg * 1000;
+               
+        }
+        public static double ConvertKilometrToMetr(double arg)
+        {
+
+            return arg * 1000;
+
+        }
+
+
+        public static int ConvertKilometrToSantimeter(int arg)
+        {
+
+            return arg * 100000;
+
+        }
+        public static double ConvertKilometrToSantimeter(double arg)
+        {
+
+            return arg * 100000;
+
+        }
+
+
+        public static int ConvertKilometrToDecimeter(int arg)
+        {
+
+            return arg * 10000;
+
+        }
+        public static double ConvertKilometrToDecimeter(double arg)
+        {
+
+            return arg * 10000;
+
+        }
+        
+
+
+        
+    }
+}
+
+/* public static void Convert_Kilometr_To(int arg)
+        {
+            var map = new Dictionary<int, int>();
+
+            map.Add(arg, arg*1000);
+            int arg1 = arg / 1000;
+            map.Add(arg1, arg*10000);
+            int arg2 = arg / 10000;
+            map.Add(arg2, arg*100000);
+            foreach (var i in map)
+        {
+          int  key = i.Key;
+          int  value = i.Value;
+          Console.WriteLine("{0} m ,{1} dm ,{2} sm ",key + "/" + value);
+        }
+         
+        }
+        public static void Convert_Kilometr_To(double arg)
+        {
+            var map = new Dictionary<double, double>();
+
+            map.Add(arg, arg * 1000);
+            map.Add(arg, arg * 10000);
+            map.Add(arg, arg * 100000);
+            foreach (var i in map)
+            {
+           double key = i.Key;
+           double value = i.Value;
+                Console.WriteLine("{0} m ,{1} dm ,{2} sm ", key + "/" + value);
+            }
+
+        }*/
---
+++ Convert/Convert/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert
+{
+    class Program :Methods
+    {
+        static void Main(string[] args)
+        {
+
+          
+
+            Console.WriteLine("Enter 1,2,3 or 4 ,where.");
+            Console.WriteLine("1.km to ...");
+            Console.WriteLine("1.sm to ...");
+            Console.WriteLine("1.dm to ...");
+            Console.WriteLine("1.sm to ...");
+            int value = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your number!");
+          double number = double.Parse(Console.ReadLine());
+          double dm = 1;
+          double sm=1;
+          double m = 1;
+            if (value == 1)
+            {
+
+             dm = Methods.ConvertKilometrToDecimeter(number);
+              m = Methods.ConvertKilometrToMetr(number);
+              sm = Methods.ConvertKilometrToSantimeter(number);
+              
+            }
+            Console.WriteLine("---------------");
+            Console.WriteLine("This is your values ! {0} dm ,{1} m,{2} sm ", dm,m,sm );
+            Console.ReadLine();
+
+            
+
+        }
+    }
+}
+
+
+
+/*  Console.WriteLine("Enter 1,2,3 or 4 ,where.");
+            Console.WriteLine("1.km to ...");
+            Console.WriteLine("1.sm to ...");
+            Console.WriteLine("1.dm to ...");
+            Console.WriteLine("1.sm to ...");
+            int value = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your number!");
+          double number = double.Parse(Console.ReadLine());
+            Methods.Convert_Kilometr_To(number);
+            
+            
+           Dictionary<int, Methods> students = new Dictionary<int, Methods>()
+            {
+                // other
+            };
+            */
